a54e878dd0cafcbe502d30dd92bc7d51df8468c9
wangjinjie <jinjie.wang@visionacademy.cn>
2021-12-14 18:57:55 +0800

    first commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <video autoplay loop :src="require('@/assets/video.mp4')" />
+    <audio style="display: none" autoplay :src="require('@/assets/music.mp3')"></audio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "landing-page",
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link);
+    },
+  },
+  created() {},
+};
+</script>
+
+<style>
+video {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
